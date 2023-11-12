0428a3587557af95da8c9f320d8a2e8aed0c1ed8
Rodrigo Alves <119018810+striks60@users.noreply.github.com>
2023-11-12 15:53:49 -0300

    Add files via upload

---
+++ atvjs.js
@@ -0,0 +1,12 @@
+
+var n1 = 8;
+var n2 = 7;
+var n3 = 9;
+
+var p1 = 2;
+var p2 = 1;
+var p3 = 3;
+
+var media = ( p1 * n1 + p2 * n2 + p3 * n3) / ( p1 + p2 + p3)
+
+console.log ("A media ponderada é:", media);
